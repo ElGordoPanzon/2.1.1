b01c1364f5770b37abf1a4a4fb38b456bb26a15c
jose jimenez <jimenjos000@warren.k12.in.us>
2025-02-03 07:56:42 -0500

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Box<String> box = new Box<>();
+        box.set("Hello");
+        String value = box.get();
+        System.out.println(value);
+    }
+
 }
